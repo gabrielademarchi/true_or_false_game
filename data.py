5d74affd17388ba51c36ccaa8582f81148d4b5b3
Gabriela Demarchi <64430799+gabrielademarchi@users.noreply.github.com>
2022-03-20 19:18:34 -0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,82 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The vapor produced by e-cigarettes is actually water.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The sum of all the numbers on a roulette wheel is 666.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The word &quot;news&quot; originates from the first letters of the 4 main directions on a compass (North, East, West, South).",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Cucumbers are usually more than 90% water.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "You are allowed to sell your soul on eBay.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The term &quot;Spam&quot; came before the food product &quot;Spam&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Kissing someone for one minute burns about 2 calories.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Albert Einstein had trouble with mathematics when he was in school.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Haggis is traditionally ate on Burns Night.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Francis Bacon died from a fatal case of pneumonia while he was attempting to preserve meat by stuffing a chicken with snow.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
